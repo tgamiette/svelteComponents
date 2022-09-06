e1d9bb4a14248206cd21e8672281fafb04174868
Sevangel <sevangelaad@gmail.com>
2022-09-06 10:33:01 +0200

    Calendar/Checkbox

---
+++ src/ui/Checkbox.svelte
@@ -0,0 +1,13 @@
+<script>
+    let isActive = false
+
+    const check = () => {
+        isActive = !isActive;
+    };
+
+</script>
+
+<form>
+    <input type="checkbox" id="confirm" name="confirm" on:click={ check }>
+    <label for="confirm">J'accepte les termes et les conditions !</label>
+</form>
