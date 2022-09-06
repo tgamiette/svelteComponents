2d0584273e8e1927f02fb00e0d1f7b616c6b9891
Teddy Gamiette <tgamiette@kernix.com>
2022-09-06 14:22:36 +0200

    sass

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,35 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import AnimateButton from "./ui/AnimateButton.svelte";
 </script>
 
+
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <AnimateButton animation="slide-left" type="submit" label="slide-left"/>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <AnimateButton animation="slide-right" type="submit" label="slide-right"/>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <AnimateButton animation="slide-blob" type="submit" label="slide-blob"/>
 </main>
 
 <style lang="scss">
-    $color: purple;
+  $color: purple;
 
-    .logo {
+  .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
+
   .logo.svelte:hover {
     filter: drop-shadow(0 0 2em #ff3e00aa);
   }
+
   .read-the-docs {
     color: #888;
   }
---
+++ src/ui/AnimateButton.svelte
@@ -0,0 +1,181 @@
+<script>
+  export let color = '#ec1313'
+  export let type = ''
+  export let label = 'Ceci est un bouton'
+  export let borderRadius = '1rem'
+  export let animation = "slide-right"
+  export let nbBlobs = 3
+</script>
+
+
+<button class="btn"
+        class:btn-slide-right={ animation === 'slide-right'}
+        class:btn-slide-left={ animation === 'slide-left'}
+        class:btn-slide-blob={ animation === 'slide-blob'}
+        type={type}
+        style="text-align:center ;">{label}
+
+  {#if animation === 'slide-blob'}
+    <span class="blob-btn__inner">
+      <span class="blob-btn__blobs">
+    {#each Array(nbBlobs) as _ , index(index)}
+      <span class="blob-btn__blob"></span>
+    {/each}
+</span></span>
+  {/if}
+</button>
+
+
+<style lang="scss">
+  $btn_color: #234db6;
+  $color: #7a2424;
+  $first_color: #FFF;
+
+  .btn {
+    font-family: 'Open Sans Condensed', sans-serif;
+    position: relative;
+    display: inline-block;
+    padding: .75rem 1.25rem;
+    border-radius: 10rem;
+    color: $first_color;
+    transition: all .3s;
+    font-size: 2rem;
+    background-color: $btn_color;
+    overflow: hidden;
+    z-index: 1;
+  }
+
+  .btn-slide-right {
+    &:before {
+      content: '';
+      position: absolute;
+      border-radius: 0 100% 100% 0;
+      bottom: 0;
+      left: 0;
+      width: 10%;
+      height: 100%;
+      transition: all 1s;
+      background-color: darken($color, 10%);
+      z-index: -1;
+    }
+
+    &:hover {
+      color: #646cff;
+
+      &:before {
+        width: 100%;
+        border-radius: 0 25rem 25rem 0;
+        transition: all 1s;
+      }
+
+      &:active {
+        color: red;
+        border-color: red;
+        //color: $first_color;
+      }
+
+    }
+  }
+
+  .btn-slide-left {
+
+    &:before {
+      content: '';
+      position: absolute;
+      border-radius: 100% 0 0 100%;
+      bottom: 0;
+      right: 0;
+      width: 10%;
+      height: 100%;
+      transition: all 1s;
+      background-color: darken($color, 10%);
+      z-index: -1;
+    }
+
+    &:hover {
+      color: #646cff;
+
+      &:before {
+        width: 100%;
+        transition: all 1s;
+        border-radius: 25rem 0 0 25rem;
+      }
+
+      &:active {
+        color: red;
+        //color: $first_color;
+      }
+
+    }
+  }
+
+  .btn-slide-left {
+    &:before {
+      content: '';
+      position: absolute;
+      border-radius: 100% 0 0 100%;
+      bottom: 0;
+      right: 0;
+      width: 10%;
+      height: 100%;
+      transition: all 1s;
+      background-color: darken($color, 10%);
+      z-index: -1;
+    }
+
+    &:hover {
+      color: #646cff;
+
+      &:before {
+        width: 100%;
+        transition: all 1s;
+        border-radius: 25rem 0 0 25rem;
+      }
+
+      &:active {
+        color: red;
+        //color: $first_color;
+      }
+
+    }
+  }
+
+  // blob
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  $openSans: 'Open Sans', Helvetica, Arial, sans-serif;
+  body {
+    background: #333;
+    font-family: $openSans;
+  }
+
+  $cyan: #0505A9;
+  $dark: #FFFFFF;
+  $borderW: 2px;
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  $openSans: 'Open Sans', Helvetica, Arial, sans-serif;
+  body {
+    background: #333;
+    font-family: $openSans;
+  }
+
+  .buttons {
+    margin-top: 50px;
+    text-align: center;
+    border-radius:30px;
+  }
+
+  $cyan: #0505A9;
+  $dark: #FFFFFF;
+  $borderW: 2px;
+
+</style>
