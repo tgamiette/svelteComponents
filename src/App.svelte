a546945818f3b721a6b7c1029bcec71cc0c749c6
Teddy Gamiette <tgamiette@kernix.com>
2022-09-07 10:20:47 +0200

    sass

--- src/App.svelte
+++ src/App.svelte
@@ -39,14 +39,6 @@
     <Dropdown />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-
   <ModalAlert />
 
   <Carrousel
