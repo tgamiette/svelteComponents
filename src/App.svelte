e0e3344f76af7ef840abf083e55d03de3222178a
Teddy Gamiette <tgamiette@kernix.com>
2022-09-07 10:10:15 +0200

    sass

--- src/App.svelte
+++ src/App.svelte
@@ -12,19 +12,6 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
 
   <div class="checkboxList">
     <CheckboxList />
